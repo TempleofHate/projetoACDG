2a17eea0792ff043d4a83917bd8b753f969e38b2
unknown <jplord284@gmail.com>
2022-06-03 17:09:25 -0300

    comentario

---
+++ desafio005.py
@@ -0,0 +1,9 @@
+import array
+numero = []
+numero.append(int(input('Digite um valor: ')))
+numero.append(int(input('Digite um valor: ')))
+numero.append(int(input('Digite um valor: ')))
+numero.append(int(input('Digite um valor: ')))
+
+for i in numero:
+    print(i*-1)
